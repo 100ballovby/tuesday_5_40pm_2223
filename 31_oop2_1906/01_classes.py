67c7373dcc3d82dd85b13dd47946f4c98bca2a10
greatraksin <greatraksin@icloud.com>
2023-06-19 17:27:08 +0300

    Adding oop lesson2

--- 31_oop2_1906/01_classes.py
+++ 31_oop2_1906/01_classes.py
@@ -1,4 +1,22 @@
 from oop_module import *
 
 
+jane = Student('Jane', 20)
+bill = Student('Bill', 21)
+jill = Student('Jill', 22)
+
+jane.set_grade(5)
+jane.set_grade(7)
+jane.set_grade(9)
+
+bill.set_grade(9)
+bill.set_grade(6)
+
+jill.set_grade(4)
+jill.set_grade(6)
+jill.set_grade(2)
+
+print(f'{jill.name} middle mark: {jill.get_average_grade()}')
+print(f'{jane.name} middle mark: {jane.get_average_grade()}')
+print(f'{bill.name} middle mark: {bill.get_average_grade()}')
 
--- 31_oop2_1906/oop_module.py
+++ 31_oop2_1906/oop_module.py
@@ -2,8 +2,35 @@ class Student:
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
-        self.grades = []
+        self.__grades = []  # инкапсулированные данные, то есть они спрятаны от внешней программы
+
+    def get_grades(self):
+        """
+        метод-геттер, позволяет получить информацию,
+        которая была инкапсулирована (сокрыта)
+        :return: None
+        """
+        for i in range(len(self.__grades)):
+            print(f"Grade {i}: {self.__grades[i]}")
+        return self.__grades
+
+    def set_grade(self, grade):
+        self.__grades.append(grade)
+
+    def get_average_grade(self):
+        summ = 0
+        for i in range(len(self.__grades)):
+            summ += self.__grades[i]
+        mark = summ / len(self.__grades)
+        return mark
 
 
-
+if __name__ == '__main__':
+    s = Student('Igor', 15)
+    s.set_grade(5)
+    s.set_grade(3)
+    s.set_grade(4)
+    s_grades = s.get_grades()
+    s_mid = s.get_average_grade()
+    print(f'Middle mark: {s_mid}')
 
