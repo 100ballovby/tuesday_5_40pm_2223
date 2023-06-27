3a6669559f84810069bb0ab4b434afe580eeace7
greatraksin <greatraksin@icloud.com>
2023-06-27 15:21:44 +0300

    Adding inheritance

---
+++ 33_inheritance_2706/03_task_soldiers.py
@@ -0,0 +1,51 @@
+class Person:
+    count = 0
+    def __init__(self, c):
+        self.id = Person.count  # id будет увеличиваться с созданием нового персонажа
+        Person.count += 1
+        self.command = c
+
+
+class Hero(Person):
+    def __init__(self, c):
+        super().__init__(c)
+        self.level = 1
+    def upLevel(self):
+        self.level += 1
+
+
+class Soldier(Person):
+    def __init__(self, c):
+        super().__init__(c)
+        self.myHero = None
+
+    def follow(self, hero):
+        self.myHero = hero.id
+
+
+h1 = Hero(1)
+h2 = Hero(2)
+
+army1 = []
+army2 = []
+
+import random as r
+
+for i in range(20):
+    n = r.randint(1, 2)
+    if n == 1:
+        army1.append(Soldier(n))
+    else:
+        army2.append(Soldier(n))
+
+print(len(army1), len(army2))
+
+if len(army1) > len(army2):
+    h1.upLevel()
+else:
+    h2.upLevel()
+
+
+army1[0].follow(h1)
+print(army1[0].id, h1.id)
+
